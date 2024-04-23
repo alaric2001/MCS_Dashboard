c04c30de09e607ba4d27e10074bf0934acbb3d4e
alaric2001 <alaric2001ra@gmail.com>
2024-04-23 22:14:15 +0700

    Add: Tailwind Css

---
+++ mcs_dashboard/resources/views/home.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    @vite('resources/css/app.css')
+</head>
+<body>
+    <div class="flex justify-center">
+        <img src="{{ asset('images/Lambang_TNI_AD.png') }}" class="" alt="">
+    </div>
+</body>
+</html>
