54328b0066ee369bcc9f26974a5ec69cee6e8fc2
alaric2001 <alaric2001ra@gmail.com>
2024-05-04 10:03:57 +0700

    Update: home page

--- mcs_dashboard/resources/views/tni/home.blade.php
+++ mcs_dashboard/resources/views/tni/home.blade.php
@@ -5,8 +5,8 @@
 @section('content')
     <div class="mx-[42px]">
         <div class="rounded-2xl bg-[#EAE5DD] h-[280px] mt-[30px] flex justify-between">
-            <div class="ml-[40px] my-[40px]">
-                <p class="font-bold text-3xl mb-3 text-[#394C29]">
+            <div class="ml-[40px] my-[40px] flex flex-col gap-4">
+                <p class="font-bold text-3xl text-[#394C29]">
                     SELAMAT DATANG DI MONITORING <br>
                     SISTEM KOMUNIKASI TNI.
                 </p>
@@ -14,7 +14,7 @@
                     Aplikasi ini akan membantu Anda untuk memonitoring <br>
                     penggunaan MCS Dan juga pelaporan gangguan layanan
                 </p>
-                <button class="bg-[#394C29] font-bold rounded-xl px-4 py-3 text-white mt-[30px]">Laporan Gangguan</button>
+                <a href="/gangguan" class="bg-[#394C29] font-bold rounded-xl px-4 py-3 w-[192px] text-white ">Laporan Gangguan</a>
             </div>
             <div class="w-[40%] flex items-end ">
                 <img class="rounded-2xl" src="{{ asset('images/bg.png') }}" alt="bg">
