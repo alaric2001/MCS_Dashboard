528b50f72d4c17a48321b3d9f74d3a6069b59aee
alaric2001 <alaric2001ra@gmail.com>
2024-05-04 09:22:36 +0700

    Add: Form modal

--- mcs_dashboard/resources/views/tni/laporan.blade.php
+++ mcs_dashboard/resources/views/tni/laporan.blade.php
@@ -11,7 +11,7 @@
                     <i class="material-icons">filter_list</i>
                     <p>Filter</p>
                 </button>
-                <button class="flex justify-between items-center  px-4 py-3 border border-[#394C29] text-white bg-[#394C29] h-[44px] rounded-xl hover:bg-[#4c6637]">
+                <button id="openModalButton" class="flex justify-between items-center  px-4 py-3 border border-[#394C29] text-white bg-[#394C29] h-[44px] rounded-xl hover:bg-[#4c6637]">
                     + Buat Laporan
                 </button>
             </div>
@@ -40,9 +40,91 @@
                         <td>-</td>
                     </tr>
                     @endforeach 
-                    
                 </tbody>
             </table>
         </div>
     </div>
+
+    <!-- Modal -->
+    <div id="myModal" class="modal hidden fixed top-0 left-0 w-full h-full flex items-center justify-center">
+        {{-- <div id="modalOverlay" class="modal-overlay fixed top-0 left-0 w-full h-full bg-black opacity-50"></div>
+        <div id="modalOverlay" class="modal-container fixed top-0 left-0 z-50 w-full h-full flex items-center justify-center"> --}}
+            <div class="modal-content w-[440px] bg-white p-8 rounded-lg shadow-md m-auto h-auto">
+                <div class="flex justify-between items-center">
+                    <h1 class="text-lg font-semibold mb-4">Tambah Laporan</h1>
+                    <button id="closeModalButton" class="p-2">
+                        <h1 class="text-lg font-semibold mb-4">x</h1>
+                    </button>
+                </div>
+                
+                <form action="/inputlaporan" method="POST">
+                    @csrf
+                    <div class="mb-4">
+                        <label for="data_mcs_id" class="block text-sm font-medium text-gray-700">Data MCS ID</label>
+                        <input type="text" name="data_mcs_id" id="data_mcs_id" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
+                    </div>
+                    <div class="mb-4">
+                        <label for="pic" class="block text-sm font-medium text-gray-700">PIC</label>
+                        <input type="text" name="pic" id="pic" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
+                    </div>
+                    <div class="mb-4">
+                        <label for="no_hp_pic" class="block text-sm font-medium text-gray-700">Nomor HP PIC</label>
+                        <input type="text" name="no_hp_pic" id="no_hp_pic" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
+                    </div>
+                    <div class="mb-4">
+                        <label for="ket" class="block text-sm font-medium text-gray-700">Keterangan</label>
+                        <textarea name="ket" id="ket" rows="3" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"></textarea>
+                    </div>
+                    <div class="flex justify-end">
+                        <div>
+                            <a href="/gangguan" id="closeModalButton" class="bg-[#6C757D] text-white px-4 py-2 rounded-md hover:bg-[#52595e]">Batal</a>
+                            <button type="submit" class="bg-indigo-500 text-white px-4 py-2 rounded-md hover:bg-indigo-600">Simpan</button>
+                        </div>
+                        
+                    </div>
+                </form>
+                {{-- <button id="closeModalButton" class="absolute top-0 right-0 m-4 text-gray-700 hover:text-gray-900">
+                    <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
+                    </svg>
+                </button> --}}
+            </div>
+        </div>
+    </div>
+
+    <script>
+        // Open modal when button clicked
+        document.getElementById('openModalButton').addEventListener('click', function() {
+            document.getElementById('myModal').classList.remove('hidden');
+        });
+
+        // Close modal when close button clicked
+        document.getElementById('closeModalButton').addEventListener('click', function() {
+            document.getElementById('myModal').classList.add('hidden');
+        });
+
+        const modalOverlay = document.getElementById('modalOverlay');
+        const modal = document.getElementById('myModal');
+
+        // Event listener untuk menutup modal saat overlay diklik
+        // modalOverlay.addEventListener('click', function() {
+        //     modalOverlay.classList.add('hidden');
+        //     modal.classList.add('hidden');
+        // });
+
+        
+        // Event listener untuk mencegah event klik overlay propagating ke modal
+        // modal.addEventListener('click', function(event) {
+        //     event.stopPropagation();
+        // });
+
+        window.onclick = function(event) {
+            if (event.target == myModal) {
+                document.getElementById('myModal').classList.add('hidden');
+            }
+        //     document.getElementById('closeModalButton').addEventListener('click', function() {
+        //     document.getElementById('myModal').classList.add('hidden');
+        // });
+        }
+    </script>
 @endsection
