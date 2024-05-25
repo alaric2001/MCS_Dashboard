8af6dac7a94b40d62e64d2bed185843bf4cf90ed
alaric2001 <alaric2001ra@gmail.com>
2024-05-25 08:56:23 +0700

    Update: Navbar for loggedin user

--- mcs_dashboard/resources/views/profile/edit_raw.blade.php
+++ mcs_dashboard/resources/views/profile/edit_raw.blade.php
@@ -7,11 +7,6 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
-            <form method="POST" action="{{ route('logout') }}" class="py-2 px-2 font-semibold ">
-                @csrf
-                <button type="submit" class="w-full py-2 bg-[#baa785] hover:bg-[#82755d] hover:text-[#FFE5B6] rounded-md">Logout</button>
-            </form>
-            
             <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
                 <div class="max-w-xl">
                     @include('profile.partials.update-profile-information-form')
--- mcs_dashboard/resources/views/tni/navbar.blade.php
+++ mcs_dashboard/resources/views/tni/navbar.blade.php
@@ -1,7 +1,27 @@
 <div class="flex justify-end">
-    <a href="/profile" class="flex items-center gap-2">
+    {{-- <a href="/profile" class="flex items-center gap-2">
         <div class="h-9 w-9 bg-[#8A7F56] rounded-[50%]"></div>
-        {{-- <img class="h-9 w-9 object-cover rounded-[50%]" src="{{ asset('images/pakbos1.jpg') }}" alt="profile"> --}}
         User
-    </a> 
+    </a>  --}}
+    {{-- <img class="h-9 w-9 object-cover rounded-[50%]" src="{{ asset('images/pakbos1.jpg') }}" alt="profile"> --}}
+
+    <div class="flex items-center gap-2">
+        @guest
+            <a href="/login" class="rounded-md p-2 border border-black text-black hover:bg-[#dcc69e] text-sm">
+                Log in
+            </a>
+            <a href="/register" class="rounded-md p-2 bg-[#3d372b] border border-[#3d372b] text-[#FFE5B6] hover:bg-[#25211a] text-sm">
+                Register
+            </a>
+        @endguest
+        @auth
+            {{-- <div class="bg-[#00000050] w-0.5 h-6 mx-3"></div> --}}
+            <a href="/profile" class="flex items-center gap-2">
+                <div class="h-9 w-9 bg-[#8A7F56] rounded-[50%]"></div>
+                {{-- <img class="h-9 w-9 object-cover rounded-[50%]" src="{{ asset('images/'. Auth::user()->user_foto) }}" alt=""> --}}
+                <p class="">{{ Auth::user()->name }}</p>
+            </a> 
+        @endauth
+    </div>
+
 </div>
